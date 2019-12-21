15b4291e4e3f92cf26b173f1fd3258bbac6ac1e5
BrunoASMauricio <brunoasmauricio@gmail.com>
2019-12-21 16:00:29 +0000

    Getting IDCode returns reverse binary, button and led on/off work but on button press the led turns off if on (quick fix it)

--- src/main.cpp
+++ src/main.cpp
@@ -1,356 +1,230 @@
 #include <Arduino.h>
-
 /*
-Project Specs
-Authors: André Sousa (up201504227) e Bruno Maurício (up201604107)
-To send to: hsm@fe.up.pt
-subject: SELE_T4_B01
-content: JTAG: https://git.fe.up.pt/up201604107/final_jtag_t4_b01   SYNC: ........TO ADD........
-Functionalities:
-If the D command is issued through the Serial input, the IDCODE from the device should be send to the Serial output
-If the B command is issued through the Serial input, whether the button is pressed or not, should be send to the Serial output
-If the 1 command is issued through the Serial input, the LED should be turned ON
-If the 0 command is issued through the Serial input, the LED should be turned OFF
+Logic Analyzer
+Anotations
+Timing Marker Pair
+|A1 - A2| = ###
+
+8MS/s for 1 s
+TCK rising edge
+
+Analyzers:
+JTAG
+TAP initial: Run-Test/Idle
+LSB first
+show TDI TDO count
+
+Async Serial -> tx
 */
-//TCK on period in microseconds/2   (to read or perform other operations in between 2 sample delays)
-#define SampleDelay 0.5
-#define maxBSRelements 64
+
 //uC pins
 #define TMS 8  //output
 #define TCK 9  //output
 #define TDI 10 //input (pic32 TD0)
 #define TDO 11 //output (pic32 TDI)
+#define LED 13 //built-in led
 
-//Bitwise Utilities
-#define Clear_Bit(Address, Bit) Address &= ~(1<<Bit)
-#define Set_Bit(Address, Bit) Address |= 1<<Bit
-#define Check_Bit(Address, Bit) ((Address & (1<<Bit))>>Bit)
-
-//TAP states
-#define TestLogicReset 0
-#define RunTestIdle 1
-
-#define SelectDrScan 2
-#define CaptureDR 3
-#define ShiftDR 4
-#define Exit1DR 5
-#define PauseDR 6
-#define Exit2DR 7
-#define UpdateDR 8
-
-#define SelectIRScan 9
-#define CaptureIR 10
-#define ShiftIR 11
-#define Exit1IR 12
-#define PauseIR 13
-#define Exit2IR 14
-#define UpdateIR 15
-
-#define GetState 16
-
-//JTag specific defines
-//IR codes
 #define BYPASS 0x1F         //Used for bypassing a device in a test chain; this allows the testing of off-chip circuitry and board level interconnections.
 #define HIGHZ 0x00          //Places device in a high-impedance state, all pins are forced to inputs
 #define SAMPLE_PRELOAD 0x02 //Samples all pins or loads a specific value into output latch. Captures the I/O states of the component, providing a snapshot of its operation.
 #define EXTEST 0x06         //Boundary scan. Allows the external circuitry and interconnections to be tested, by either forcing various test patterns on the output pins, or capturing test results from the input pins.
 #define IDCODE 0x01         //Shifts out the device’s ID code
 
-//Set the first 4 bits of ISR
-#define setIR(ir_in) IR = ir_in&0x0f
-#define GoToLogicReset() while(TAP(1) != TestLogicReset){ TAP(1); }
+uint8_t byte_array[148];
+uint16_t iter;               //Multipurpose iterator
+uint16_t iter2;               //Multipurpose iterator
 
-uint8_t IR;             //Instruction Register. ONLY 4 bits, LSb first
-uint32_t DEVID;         //The devices' 32-bit part identifier
-uint32_t BSRAmmount;    //Ammount of BSR elements
-uint64_t TDOBuffer;     //64 bit Buffer that holds the current TDO output
-uint64_t TDIBuffer;     //64 bit Buffer that holds the received TDI input
-
-int iter;               //Multipurpose iterator
-//------------------------------ GENERAL Purpose ------------------------------
-/*
-On error, send a message through the Serial
-*/
-void ERROR(){
-}
-/*
-Simulates and controls the device TAP controller
-*/
-uint8_t TAP(uint8_t next_bit){
-  static uint8_t state = 0;
-  uint8_t activate = 1;
-  switch(state){
-    case TestLogicReset:
-      if(next_bit){
-        state = TestLogicReset;
-      }else{
-        state = RunTestIdle;
-      }
-      break;
-    case RunTestIdle:
-      if(next_bit){
-        state = SelectDrScan;
-      }else{
-        state = RunTestIdle;
-      }
-      break;
-    case SelectDrScan:
-      if(next_bit){
-        state = SelectIRScan;
-      }else{
-        state = CaptureDR;
-      }
-      break;
-    case CaptureDR:
-      if(next_bit){
-        state = Exit1DR;
-      }else{
-        state = ShiftDR;
-      }
-      break;
-    case ShiftDR:
-      if(next_bit){
-        state = Exit1DR;
-      }else{
-        state = ShiftDR;
-      }
-      break;
-    case Exit1DR:
-      if(next_bit){
-        state = UpdateDR;
-      }else{
-        state = PauseDR;
-      }
-      break;
-    case PauseDR:
-      if(next_bit){
-        state = Exit2DR;
-      }else{
-        state = PauseDR;
-      }
-      break;
-    case Exit2DR:
-      if(next_bit){
-        state = UpdateDR;
-      }else{
-        state = ShiftDR;
-      }
-      break;
-    case UpdateDR:
-      if(next_bit){
-        state = SelectDrScan;
-      }else{
-        state = RunTestIdle;
-      }
-      break;
-    case SelectIRScan:
-      if(next_bit){
-        state = TestLogicReset;
-      }else{
-        state = CaptureIR;
-      }
-      break;
-    case CaptureIR:
-      if(next_bit){
-        state = Exit1DR;
-      }else{
-        state = ShiftIR;
-      }
-      break;
-    case ShiftIR:
-      if(next_bit){
-        state = Exit1IR;
-      }else{
-        state = ShiftIR;
-      }
-      break;
-    case Exit1IR:
-      if(next_bit){
-        state = UpdateIR;
-      }else{
-        state = PauseIR;
-      }
-      break;
-    case PauseIR:
-      if(next_bit){
-        state = Exit2IR;
-      }else{
-        state = PauseIR;
-      }
-      break;
-    case Exit2IR:
-      if(next_bit){
-        state = UpdateIR;
-      }else{
-        state = ShiftIR;
-      }
-      break;
-    case UpdateIR:
-      if(next_bit){
-        state = SelectDrScan;
-      }else{
-        state = RunTestIdle;
-      }
-      break;
-    case GetState:
-      activate = 0;
-      break;
-    default:
-      ERROR();
-      break;
-  }
-  if(activate){
-    digitalWrite(TCK,LOW);
-    digitalWrite(TMS,next_bit);
-    digitalWrite(TCK,HIGH);
-    delayMicroseconds(2*SampleDelay);
-    digitalWrite(TCK,LOW);
-  }
-  return state;
-}
-/*
-Sets TAP to a specified destination state
-*/
-void setTAP(uint8_t destination_state){
-  uint16_t path;
-  uint8_t path_length;
-  if(TAP(GetState) != destination_state){           //Check if TAP is already at the destination
-    GoToLogicReset();
-    switch(destination_state){
-      case ShiftIR:
-        path = 0b00110;
-        path_length = 5;
-        break;
-      case ShiftDR:
-        path = 0b0010;
-        path_length = 4;
-        break;
-      default:
-        ERROR();
-        break;
-    }
-    for(iter = 0; iter < path_length; iter++){
-      TAP(path&0x01);
-      path = path >> 1;
-    }
+void print(const char* message, int ammount){
+  if(ammount == -1){
+    Serial.println(message);
+  }else{
+    Serial.print(message);
+    Serial.println(ammount);
   }
 }
-/*
-Write ammount bits from TDO, and reads ammount of bits into TDI
-Clears TDO in the process
-An iterable by TMS 0 state (RunTestIdle, ShiftDR, ShiftIR, PauseDr or PauseIR) is assumed
-LSb first
-*/
-void communicate(uint8_t ammount){
-  if(ammount > 64){
-    ERROR();
-    return;
+void print_bin(const char* message,uint16_t in){
+  Serial.println(message);
+  for(iter2 = 0; iter2 < 16; iter2++){
+    Serial.write((char)(in>>iter2&0x01)+48);
   }
+  Serial.println();
+}
+void clock() {
+  digitalWrite(TCK, HIGH);
+  delayMicroseconds(2);
   digitalWrite(TCK, LOW);
-  digitalWrite(TMS, LOW);
-  for(iter = 0; iter < ammount; iter++){
-    digitalWrite(TDO, TDOBuffer&0x01);
-    digitalWrite(TCK, HIGH);
-    delayMicroseconds(SampleDelay);
-    Set_Bit(TDIBuffer,digitalRead(TDI));
-    delayMicroseconds(SampleDelay);
-    digitalWrite(TCK, LOW);
-    TDOBuffer = TDOBuffer>>1;
-  }
+  delayMicroseconds(2);
 }
-/*
-Writes a certain ammount of data into TDOBuffer, LSb first
-*/
-void setTDO(uint64_t data, uint8_t ammount){
-  for(iter = 0; iter < ammount; iter++){
-    if(Check_Bit(data,iter)){
-      Set_Bit(TDOBuffer,iter);
-    }else{
-      Clear_Bit(TDOBuffer,iter);
-    }
-  }
-}
-/*
-Reads a certain ammount of TDIBuffer into data
-*/
-void getTDI(uint64_t* data, uint8_t ammount){
-  for(iter = 0; iter < ammount; iter++){
-    if(Check_Bit(TDIBuffer,iter)){
-      Set_Bit(*data,iter);
-    }else{
-      Clear_Bit(*data,iter);
-    }
-  }
-}
-//------------------------------ JTAG functions ------------------------------
-/*
-Load Instruction Register with the scpecified Instruction Register Code
-*/
-void loadIR(uint8_t IRC){
-  if(IR != IRC){            //Check if the IRC is already loaded in the IR
-    setTAP(ShiftIR);        //Set TAP to ShiftIR
-    setIR(IRC);             //Prepare and output IRC
-    setTDO(IR,4);
-    communicate(4);
-  }
-}
-/*
-Load Data Register with the scpecified ammount of data
-*/
-void loadDR(uint64_t data, uint8_t ammount){
-  setTAP(ShiftDR);        //Set TAP to ShiftIR
-  setTDO(data,ammount);   //Output data
-  communicate(ammount);
-}
-//------------------------------ Functions ------------------------------
-/*
-Test Boundary Scan Register integrity
-*/
-uint8_t testJTAG(){
-  uint64_t data;
-  GoToLogicReset();
-  //Test bypass bit
-  loadIR(BYPASS);
-  setTDO(1,1);          //Clear TDOBuffer
-  communicate(3);       //If everything is ok, should receive 01
-  getTDI(&data,1);
-  if(data != 1){
-    ERROR();
-  }
-  //Test BSR
-  loadIR(EXTEST);
-  setTDO(1,1);          //Clear TDOBuffer
-  communicate(3);       //If everything is ok, should receive 01
-  getTDI(&data,1);
-  if(data != 1){
-    ERROR();
-  }
-
-  return 1;
+void advanceCLK(int tdo, int tms){
+  digitalWrite(TDO, tdo);
+  digitalWrite(TMS,tms);
+  clock();
 }
 
-uint32_t detectBSRAmmount(){
-  GoToLogicReset();
-
-  return BSRAmmount;
-}
 
 void setup() {
   Serial.begin(9600);
-  setIR(0x00);
-  DEVID = 0;
-  BSRAmmount = 0;    
-  TDOBuffer = 0;
-  TDIBuffer = 0;
+  print("Starting\n\n",-1);
+  pinMode(TDI,INPUT);     //input (pic32 TD0)
   pinMode(TMS,OUTPUT);
   pinMode(TCK,OUTPUT);
-  pinMode(TDI,INPUT);     //input (pic32 TD0)
   pinMode(TDO,OUTPUT);    //input (pic32 TDI)
   digitalWrite(TMS,LOW);
-  digitalWrite(TCK,LOW);
+  digitalWrite(TCK, HIGH);
   digitalWrite(TDO,LOW);
+}
+void shiftIR(unsigned int ammount, char useconst0){
+  advanceCLK(0,1);
+  advanceCLK(0,0);
+  advanceCLK(0,0);
+  if(useconst0){
+    for(iter = 0; iter < ammount-1; iter++){
+      advanceCLK(0,0);
+      byte_array[iter] = ((char)digitalRead(TDI))+'0';
+      Serial.print((char)byte_array[iter]);
+    }
+    advanceCLK(0,1);
+    byte_array[iter] = ((char)digitalRead(TDI))+'0';
+    Serial.print((char)byte_array[iter]);
+
+  }else{
+    for(iter = 0; iter < ammount-1; iter++){
+      advanceCLK(byte_array[iter]=='1',0);
+      byte_array[iter] = ((char)digitalRead(TDI))+'0';
+    }
+    advanceCLK(byte_array[iter]=='1',1);
+    byte_array[iter] = ((char)digitalRead(TDI))+'0';
+  }
+  advanceCLK(0,1);
+  advanceCLK(0,1);
+}
+void shiftDR(unsigned int ammount, char useconst0){
+  advanceCLK(0,0);
+  advanceCLK(0,0);
+  char temp;
+  if(useconst0){
+    for(iter = 0; iter < ammount-1; iter++){
+      byte_array[iter] = ((char)digitalRead(TDI))+'0';
+      advanceCLK(0,0);
+    }
+    byte_array[iter] = ((char)digitalRead(TDI))+'0';
+    advanceCLK(0,1);
+  }else{
+    for(iter = 0; iter < ammount-1; iter++){
+      temp = ((char)digitalRead(TDI))+'0';
+      advanceCLK(byte_array[iter]=='1',0);
+      byte_array[iter] = temp;
+    }
+    temp = ((char)digitalRead(TDI))+'0';
+    advanceCLK(byte_array[iter]=='1',1);
+    byte_array[iter] = temp;
+  }
+  advanceCLK(0,1);
+  advanceCLK(0,1);
 }
 
 void loop() {
-  testBSR();
+  if(Serial.available()){
+    switch(Serial.read()){//ia is returning correctly
+      case 'r'://reset
+        digitalWrite(TMS,LOW);
+        digitalWrite(TCK, HIGH);
+        digitalWrite(TDO,LOW);
+        for(int i = 0; i < 6; i++){
+          advanceCLK(0,1);
+        }
+        advanceCLK(0,0);
+        advanceCLK(0,1);
+        Serial.println("Initialized");
+      break;
+      case 'a'://IDCODE: 1001 0100 0000 1010 0001 0010 0001 0100
+        //DEVICE ID 0A 0000 1010
+        byte_array[0] = '1';
+        byte_array[1] = '0';
+        byte_array[2] = '0';
+        byte_array[3] = '0';
+        byte_array[4] = '0';
+        shiftIR(5,0);
+        shiftDR(32,1);
+        for(int i = 0; i < 32; i++) Serial.print((char)byte_array[i]);
+        Serial.println();
+      break;
+      case 'b'://Button
+        //
+        byte_array[0] = '0';
+        byte_array[1] = '1';
+        byte_array[2] = '0';
+        byte_array[3] = '0';
+        byte_array[4] = '0';
+        shiftIR(5,0);
+        shiftDR(148,1);
+        for(int i = 0; i < 148; i++) Serial.print((char)byte_array[i]);
+        Serial.println();
+        Serial.println((char)byte_array[135]);
+        /*
+
+        */
+        /*
+10 1 1011011011010000111011011011011011011010011010011010010010011011010011011011010010010011011010011011011010010010011011010010011001000011101101100
+10 0 1011011011010000111011011011011011011010011010011010010010011011010011011011010010010011011010011011011010010010011011010010011001000011101101100
+
+        10 1 10110100110100001110110110111
+        10 0 10110100110100001110110110111
+        */
+       if(byte_array[3] == '1'){
+         Serial.println("ON");
+       }else{
+         Serial.println("OFF");
+       }
+      break;
+      case '1'://Turn led on  SCK2 RG6
+        byte_array[0] = '0';
+        byte_array[1] = '1';
+        byte_array[2] = '1';
+        byte_array[3] = '0';
+        byte_array[4] = '0';
+        shiftIR(5,0);
+        /*
+        advanceCLK(0,0);
+        advanceCLK(0,0);
+        
+        advanceCLK(1,0);//147
+        advanceCLK(1,0);//146
+        advanceCLK(1,1);//145
+        
+        advanceCLK(0,1);
+        advanceCLK(0,1);
+        */
+        shiftDR(148,1);
+        byte_array[137] = '1';
+        byte_array[138] = '1';
+        shiftDR(148,0);
+      break;
+      case '0'://Turn led off
+        byte_array[0] = '0';
+        byte_array[1] = '1';
+        byte_array[2] = '1';
+        byte_array[3] = '0';
+        byte_array[4] = '0';
+        shiftIR(5,0);
+        /*
+        advanceCLK(0,0);
+        advanceCLK(0,0);
+        
+        advanceCLK(0,0);//147
+        advanceCLK(1,0);//146
+        
+        advanceCLK(0,1);
+        advanceCLK(0,1);
+        */
+        shiftDR(148,1);
+        byte_array[137] = '0';
+        byte_array[138] = '1';
+        shiftDR(148,0);
+      break;
+      case 'e':
+        shiftDR(32,1);
+      break;
+    }
+  }
 }
